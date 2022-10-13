c6e71f7d3f1912f7597e8c95bf32715ffc15b391
jhogen <jakob.hogenkamp@kpnmail.nl>
2022-10-13 19:42:48 +0200

    Update app_week5.py

--- app_week5.py
+++ app_week5.py
@@ -381,8 +381,7 @@ def add_categorical_legend(folium_map, title, colors, labels):
 # In[111]:
 
 
-m = folium.Map(location=[42.416775, 5.703790], zoom_start=6, tiles='https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png',
-        attr='Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)').add_to(m)
+m = folium.Map(location=[42.416775, 5.703790], zoom_start=6).add_to(m)
 
 for index,row in df2.iterrows():
     if row['AddressInfo']['Country']['Title'] == 'Spain':
